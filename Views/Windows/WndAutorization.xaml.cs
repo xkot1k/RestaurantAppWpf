9afd1ab1c97a21f2fb2996efebcbeb62da61c92e
xkot1k <100746140+xkot1k@users.noreply.github.com>
2022-11-27 17:50:59 +0300

    RestaurantAppWpf
    RestaurantApp

---
+++ Views/Windows/WndAutorization.xaml.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace RestaurantApp
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void RadioButton_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, функция в разработке в данный момент!");
+        }
+
+        private void RadioButton_Click_1(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, функция в разработке в данный момент!");
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (txtLogin.Text == "admin" && (psbPassword.Password == "admin" || txtPassword.Text == "admin"))
+            {
+                Views.Windows.WndHome wndHome = new Views.Windows.WndHome();
+                wndHome.Show();
+                this.Close();
+            }
+            else
+            {
+                MessageBox.Show(psbPassword.Password);
+            }
+        }
+
+        private void chkShowPass_Checked(object sender, RoutedEventArgs e)
+        {
+            if (chkShowPass.IsChecked == true)
+            {
+                txtPassword.Visibility = Visibility.Visible;
+                psbPassword.Visibility = Visibility.Hidden;
+                txtPassword.Text = psbPassword.Password;
+            }
+            else
+            {
+                psbPassword.Visibility = Visibility.Visible;
+                txtPassword.Visibility = Visibility.Hidden;
+                psbPassword.Password = txtPassword.Text;
+            }
+        }
+
+        private void Grid_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            this.DragMove();
+        }
+
+        private void btnMinimize_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnExit_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+    }
+}
---
+++ Views/Windows/WndHome.xaml.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace RestaurantApp.Views.Windows
+{
+    /// <summary>
+    /// Логика взаимодействия для WndHome.xaml
+    /// </summary>
+    public partial class WndHome : Window
+    {
+        public WndHome()
+        {
+            InitializeComponent();
+        }
+
+        private void LogoContainer_MouseLeftButtonDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, функция в разработке в данный момент!");
+        }
+
+        private void btnSubmit_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, функция в разработке в данный момент!");
+        }
+
+        private void btnAddPosition_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, функция в разработке в данный момент!");
+        }
+
+        private void btnRemovePosition_Click(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, функция в разработке в данный момент!");
+        }
+
+        private void rdbMenu_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, функция в разработке в данный момент!");
+        }
+
+        private void rdbContacts_Checked(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Извините, функция в разработке в данный момент!");
+        }
+
+        private void btnMinimizedHome_Click(object sender, RoutedEventArgs e)
+        {
+            WindowState = WindowState.Minimized;
+        }
+
+        private void btnExitHome_Click(object sender, RoutedEventArgs e)
+        {
+            Environment.Exit(1);
+        }
+    }
+}
